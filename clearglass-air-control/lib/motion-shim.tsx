fd4ae868f63a816ce1b0d12cfd5c9bc037939c58
Desmond Otieno <77194670+ClearGlasslabs@users.noreply.github.com>
2026-07-06 10:10:55 -0400

    Fix air control motion shim types

--- clearglass-air-control/lib/motion-shim.tsx
+++ clearglass-air-control/lib/motion-shim.tsx
@@ -14,12 +14,16 @@ type MotionProps = Record<string, unknown> & {
 const ignoredMotionProps = new Set(['animate', 'initial', 'transition', 'whileHover', 'whileTap']);
 
 function createMotionElement(tag: keyof React.JSX.IntrinsicElements) {
-  const MotionElement = React.forwardRef<unknown, MotionProps>(({ children, ...props }, ref) => {
+  const MotionElement = React.forwardRef<Element, MotionProps>(({ children, ...props }, ref) => {
     const passthrough = Object.fromEntries(
       Object.entries(props).filter(([key]) => !ignoredMotionProps.has(key)),
     );
 
-    return React.createElement(tag, { ...passthrough, ref } as Record<string, unknown>, children);
+    return React.createElement(
+      tag as React.ElementType,
+      { ...passthrough, ref },
+      children as React.ReactNode,
+    );
   });
   MotionElement.displayName = `MotionShim.${String(tag)}`;
   return MotionElement;
